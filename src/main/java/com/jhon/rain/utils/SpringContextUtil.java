f5db7fe64b78437fd9fe72f7f9d62134b075cdc3
CoderCat <3291371805@qq.com>
2017-04-22 13:23:37 +0800

    字符编译报错

--- src/main/java/com/jhon/rain/utils/SpringContextUtil.java
+++ src/main/java/com/jhon/rain/utils/SpringContextUtil.java
@@ -6,14 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
 
-/**
- * <p>spring上下文获取工具类 〈一句话功能简述〉 〈功能详细描述〉</p>
- * 
- * @author jiangyu
- * @version v1.0
- * @see SpringContextUtil
- * @since v1.0
- */
+
 public class SpringContextUtil implements ApplicationContextAware
 {
 
@@ -41,29 +34,13 @@ public class SpringContextUtil implements ApplicationContextAware
         return applicationContext;
     }
 
-    /**
-     * <p> 功能描述：获取bean对象</p>
-     * @author jiangyu
-     * @date 2016年3月17日 下午6:59:24
-     * @param componentClass 要加载的bean类
-     * @return T 自定义的类型
-     * @since v1.0
-     */
+   
     public static <T> T getComponent(Class<T> componentClass)
     {
         return SpringContextUtil.getApplicationContext().getBean(componentClass);
     }
     
-    /**
-     * <p> 功能描述：获取bean</p>
-     * @author  jiangyu
-     * @date  2016年11月27日 下午3:24:18
-     * @param componentClass 对应的bean的组件类 
-     * @param beanName 对应的bean的名称 
-     * @return 返回componentClass对应的类型组件实体
-     * @version v1.0
-     * @since V1.0
-     */
+    
     @SuppressWarnings("unchecked")
     public static <T> T getBean(Class<T> componentClass,String beanName)
     {
