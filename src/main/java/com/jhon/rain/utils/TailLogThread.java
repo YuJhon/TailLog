8ed327504d2f182b0706b9125124153a3122330c
CoderCat <3291371805@qq.com>
2017-04-22 13:28:40 +0800

    注释去除

--- src/main/java/com/jhon/rain/utils/TailLogThread.java
+++ src/main/java/com/jhon/rain/utils/TailLogThread.java
@@ -8,14 +8,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.InputStream;
 
-/**
- * <p>查询日志的线程类</p>
- *
- * @author jiangyu
- * @version v1.0
- * @className TailLogThread
- * @create 2016-12-07 20:30
- */
+
 public class TailLogThread extends Thread {
     private BufferedReader reader;
 
@@ -29,7 +22,7 @@ public class TailLogThread extends Thread {
         try {
             TextMessage message = null;
             while ((line = reader.readLine()) != null) {
-                // 将实时日志通过WebSocket发送给客户端，给每一行添加一个HTML换行
+                
                 message = new TextMessage(line + "<br>");
                 SpringContextUtil.getComponent(TextMessageHandler.class).sendMessageToUsers(message);
             }
